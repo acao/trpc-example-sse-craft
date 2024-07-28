0dbdbbf3d4a7b8f202c8ff5c647962ede411d448
Rikki Schulte <rikki.schulte@gmail.com>
2024-07-28 18:51:39 +0200

    default imports

---
+++ src/app/channels/[channelId]/pageEditor/defaults.ts
@@ -0,0 +1,142 @@
+
+export const defaultPage = `{
+    "ROOT": {
+      "type": {
+        "resolvedName": "Container"
+      },
+      "isCanvas": true,
+      "props": {
+          "padding": 12
+      },
+      "displayName": "Container",
+      "custom": {},
+      "hidden": false,
+      "nodes": [
+        "QHkIPZk-du"
+      ],
+      "linkedNodes": {}
+    },
+    "QHkIPZk-du": {
+      "type": {
+        "resolvedName": "Container"
+      },
+      "isCanvas": false,
+      "props": {},
+      "displayName": "Container",
+      "custom": {},
+      "parent": "ROOT",
+      "hidden": false,
+      "nodes": [
+        "45ybI3y_Xe",
+        "WasvVCdsrK",
+        "lTUTUuFYry"
+      ],
+      "linkedNodes": {}
+    },
+    "45ybI3y_Xe": {
+      "type": {
+        "resolvedName": "Heading"
+      },
+      "isCanvas": true,
+      "props": {
+        "text": "Example Heading",
+        "level": "h2"
+      },
+      "displayName": "Heading",
+      "custom": {},
+      "parent": "QHkIPZk-du",
+      "hidden": false,
+      "nodes": [
+        "SH4f9mvoV6"
+      ],
+      "linkedNodes": {}
+    },
+    "SH4f9mvoV6": {
+      "type": {
+        "resolvedName": "Heading"
+      },
+      "isCanvas": false,
+      "props": {
+        "text": "Heading 1",
+        "level": "h2"
+      },
+      "displayName": "Heading",
+      "custom": {},
+      "parent": "45ybI3y_Xe",
+      "hidden": false,
+      "nodes": [],
+      "linkedNodes": {}
+    },
+    "WasvVCdsrK": {
+      "type": {
+        "resolvedName": "Text"
+      },
+      "isCanvas": true,
+      "props": {
+        "text": "Hi",
+        "fontSize": 12,
+        "color": "black",
+        "padding": 2
+      },
+      "displayName": "Text",
+      "custom": {},
+      "parent": "QHkIPZk-du",
+      "hidden": false,
+      "nodes": [
+        "ZDuRPJXxly"
+      ],
+      "linkedNodes": {}
+    },
+    "ZDuRPJXxly": {
+      "type": {
+        "resolvedName": "Text"
+      },
+      "isCanvas": false,
+      "props": {
+        "text": "Drop a card here",
+        "fontSize": 12,
+        "color": "black",
+        "padding": 2
+      },
+      "displayName": "Text",
+      "custom": {},
+      "parent": "WasvVCdsrK",
+      "hidden": false,
+      "nodes": [],
+      "linkedNodes": {}
+    },
+    "lTUTUuFYry": {
+      "type": {
+        "resolvedName": "Heading"
+      },
+      "isCanvas": true,
+      "props": {
+        "text": "Example Heading",
+        "level": "h2"
+      },
+      "displayName": "Heading",
+      "custom": {},
+      "parent": "QHkIPZk-du",
+      "hidden": false,
+      "nodes": [
+        "mx1dkHOgQK"
+      ],
+      "linkedNodes": {}
+    },
+    "mx1dkHOgQK": {
+      "type": {
+        "resolvedName": "Heading"
+      },
+      "isCanvas": false,
+      "props": {
+        "text": "Heading 2",
+        "level": "h2"
+      },
+      "displayName": "Heading",
+      "custom": {},
+      "parent": "lTUTUuFYry",
+      "hidden": false,
+      "nodes": [],
+      "linkedNodes": {}
+    }
+  }`;
